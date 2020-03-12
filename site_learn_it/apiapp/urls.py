a50883dbab2c385171518c85212f8f65eb0b2874
Yuriy Belov <paganism@mail.ru>
2020-03-12 09:27:17 +0300

    rest_api. fixed notes

--- site_learn_it/apiapp/urls.py
+++ site_learn_it/apiapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import UserDetails, UserList, GroupList
 
-# app_name = 'apiapp'
+
 urlpatterns = [
     path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')),
     path("users/", UserList.as_view()),
