02be4fc1b1c58a80da4634065adc9335b029d111
Yuriy Belov <paganism@mail.ru>
2020-03-17 19:01:18 +0300

    added graphql simple tests

--- site_learn_it/learn_it/tests.py
+++ site_learn_it/learn_it/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from graphene_django.utils.testing import GraphQLTestCase
+from site_learn_it.schema import schema
+
+
+class GQLTestCase(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+
+    def test_simple_all_users_query(self):
+        response = self.query(
+            '''
+            query {
+              allUsers(limit:4) {
+                id
+                username
+                role
+              }
+            }
+            ''',
+            op_name='allUsers'
+        )
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+
+    def test_filtered_users_query(self):
+        response = self.query(
+            '''
+            query {
+              filteredUsers (username_Istartswith: "v") {
+                edges {
+                  node {
+                    id,
+                    username,
+                    firstName,
+                    lastName
+                  }
+                }
+              }
+            }
+            ''',
+            op_name='filteredUsers'
+        )
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+
+    def test_all_courses_query(self):
+        response = self.query(
+            '''
+            query {
+              allCourses{
+                id
+                title
+                student: students (role:"s"){
+                  edges{
+                    node{
+                      username
+                    }
+                  }
+                }
+                teacher: students (role:"t"){
+                  edges{
+                    node{
+                      username
+                    }
+                  }
+                }
+              }
+            }
+            ''',
+            op_name='allCourses'
+        )
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+
