3c2fb03a9833d0362963215bd57b7f82e5d1889f
Yuriy Belov <paganism@mail.ru>
2020-04-13 18:50:22 +0300

    added auth api view

--- site_learn_it/apiapp/urls.py
+++ site_learn_it/apiapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .views import UserViewSet, CourseViewSet, GroupList
+from .views import UserViewSet, CourseViewSet, GroupList, LoginView
 
 
 urlpatterns = [
     path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # path("token/", include('oauth2_provider.urls', namespace='token')),
     path("users/", UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='apiusers'),
     path("users/<pk>", UserViewSet.as_view({'get': 'retrieve'}), name='user-detail'),
     path("groups/", GroupList.as_view()),
     path("courses/", CourseViewSet.as_view({'get': 'list', 'post': 'create'}), name='apicourses'),
     path("courses/<pk>", CourseViewSet.as_view({'get': 'retrieve'})),
+    path("login/", LoginView.as_view(), name="login"),
     ]
--- site_learn_it/apiapp/views.py
+++ site_learn_it/apiapp/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from django.contrib.auth.models import Group
 from learn_it.models import CustomUser, Course
 from rest_framework import viewsets
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, status
 from .serializers import UserSerializer, GroupSerializer, CourseSerializer
+from django.contrib.auth import authenticate
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
-
+from site_learn_it import settings
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -51,3 +55,27 @@ class CourseViewSet(viewsets.ModelViewSet):
     """
     queryset = Course.objects.all().order_by('-date_start')
     serializer_class = CourseSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        token = self.get_token(username=username, passw=password)
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": token['access_token']})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_token(self, username, passw):
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'authorization': settings.BASIC_AUTH
+        }
+        payload = {'grant_type': 'password', 'username': username, 'password': passw, }
+        token = requests.post('http://localhost:8000/api-oauth/o/token/', data=payload, headers=headers)
+        return token.json()
