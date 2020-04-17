487ab1abd5ff070930bb4643ee7055df4e9e55b1
Yuriy Belov <paganism@mail.ru>
2020-04-17 14:01:08 +0300

    added react main page

---
+++ site_learn_it/frontend/src/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+
+    render() {
+      return (
+        <div className="info">
+
+            <div class="jumbotron">
+                <h1>Track your courses</h1>
+            </div>
+                <h2><a href="/register">Register an account</a> to pass the course to improove your knowledge.</h2>
+                <h2>Whenever you learn something new about a course, don't give up.</h2>
+            </div>
+      );
+    }
+  }
+  
+  export default Main;
